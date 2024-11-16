e36a68c24ded34c0a263c01e3245c1d935c69eb0
error707-persona <areeshasayed09@gmail.com>
2024-11-16 23:23:46 +0530

    added meeting room layout and options

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,6 +1,15 @@
 import { cn } from '@/lib/utils';
-import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
+import { CallControls, CallParticipantsList, CallStatsButton, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
 import React, { useState } from 'react'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { LayoutList, Users } from 'lucide-react';
 
 type CallLaoutType = 'grid' | 'speaker-left' | 'speaker-right'
 const MeetingRoom = () => {
@@ -20,14 +29,38 @@ const MeetingRoom = () => {
         <section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
             <div className="relative flex size-full items-center justify-center">
                 <div className="flex size-full max-w-[1000px] items-center">
-                    <CallLayout/>
+                    <CallLayout />
                 </div>
-                <div className={cn('h-[calc(100vh-86px)] hidden ml-2', {'show-block':showParticipants})}>
-                    <CallParticipantsList onClose={()=>setshowParticipants(false)}/>
+                <div className={cn('h-[calc(100vh-86px)] hidden ml-2', { 'show-block': showParticipants })}>
+                    <CallParticipantsList onClose={() => setshowParticipants(false)} />
                 </div>
             </div>
             <div className='fixed bottom-0 flex w-full items-center justify-center'>
-                <CallControls/>
+                <CallControls />
+                <DropdownMenu>
+                    <div className='flex items-center'>
+                        <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg[#4c535b]">
+                            <LayoutList size={20} className='text-white' />
+                        </DropdownMenuTrigger>
+                    </div>
+                    <DropdownMenuContent className='border-none'>
+                        {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
+                            <div key={index}>
+                                <DropdownMenuItem className='cursor-pointer bg-dark-1 rounded hover:bg-gray-600 text-white' onClick={() => setlayout(item.toLowerCase() as CallLaoutType)}>
+                                    {item}
+                                </DropdownMenuItem>
+                            </div>
+                        ))}
+                        <DropdownMenuSeparator />
+
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                <CallStatsButton/>
+                        <button onClick={()=>setshowParticipants((prev)=>!prev)}>
+                            <div className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg[#4c535b]'>
+                                <Users size={20} className='text-white'/>
+                            </div>
+                        </button>
             </div>
         </section>
     )
